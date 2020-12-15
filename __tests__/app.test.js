f95becffd4a06b44b8345c2a3a541e63143b46c2
Sydney James <sydneyjames7@gmail.com>
2020-12-14 16:13:48 -0800

    parse fixed and refactored, tests passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,18 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
+  it('should GET plain text hi', async() => {
+    const response = await request(app)
+      .get('/');
 
+    expect(response.text).toEqual('hi');
+  });
+
+  it('should POST status code 200 and plain text', async() => {
+    const response = await request(app)
+      .post('/echo')
+      .send('hi');
+
+    expect(response.text).toEqual('hi');
+  });
 });
